d0d2203b57b2572d4a924b912ba54fdebfd6a6c7
Timucin <timucinema@gmail.com>
2023-02-02 15:14:32 -0600

    addr

--- main.tf
+++ main.tf
@@ -4,9 +4,14 @@ resource "aws_vpc" "example" {
     cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
     enable_dns_support = true
-    enable_dns_hostname = true 
+    enable_dns_hostnames = true 
 }
 
-output info {
-    value = aws_vpc.example
+resource "aws_subnet" "public_subnet1" {
+    depends_on = [
+        aws_vpc.example
+    ]
+  
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
 }
